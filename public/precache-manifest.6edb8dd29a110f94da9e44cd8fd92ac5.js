fc2fa92c5ca4cfc899fea79cd426821d08a2c894
sso just <528226249@qq.com>
2018-12-09 21:41:40 +0800

    人格测试海报

---
+++ public/precache-manifest.6edb8dd29a110f94da9e44cd8fd92ac5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "896ca9e2117560ea1838537de1e9de07",
+    "url": "/static/media/zhonghei.896ca9e2.ttf"
+  },
+  {
+    "revision": "21e0d0d37d165436fb808e88c9633bdc",
+    "url": "/static/media/xinliceshi5.21e0d0d3.png"
+  },
+  {
+    "revision": "9a33f354b9897ca2e83c6c5ce016033b",
+    "url": "/static/media/qrcode.9a33f354.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e3f55b3258417388cbf",
+    "url": "/static/js/main.2e3f55b3.chunk.js"
+  },
+  {
+    "revision": "bf3feaa6ec20d0b0d1a0",
+    "url": "/static/js/1.bf3feaa6.chunk.js"
+  },
+  {
+    "revision": "2e3f55b3258417388cbf",
+    "url": "/static/css/main.c4b75d3f.chunk.css"
+  },
+  {
+    "revision": "e7203f5ad3faac45c771d216c0b4c5ef",
+    "url": "/index.html"
+  }
+];
